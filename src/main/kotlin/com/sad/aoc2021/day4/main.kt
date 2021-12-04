ae9fed49860acaba93f354f9061e4ab0adbb1307
Sergey Dubakov <serge.dubakov@gmail.com>
2021-12-04 12:12:09 +0000

    day 4 part 1

---
+++ src/main/kotlin/com/sad/aoc2021/day4/main.kt
@@ -0,0 +1,99 @@
+package com.sad.aoc2021.day4
+
+import com.sad.aoc2021.loadFromResources
+import com.sad.aoc2021.splitWhen
+import java.lang.IllegalStateException
+
+interface MarkingStatus
+
+object NotFullyMarked : MarkingStatus
+
+data class RowMarked(val row: Int) : MarkingStatus
+
+data class ColumnMarked(val col: Int) : MarkingStatus
+
+data class Result(val number: Int, val grid: List<List<Int>>, val gridState: List<MutableList<Boolean>>, val markingStatus: MarkingStatus)
+
+fun findWinningGrid(numbers: List<Int>, grids: List<List<List<Int>>>): Result {
+    val states: List<List<MutableList<Boolean>>> = grids.map { it.map { it.map { false }.toMutableList() } }
+    for (num in numbers) {
+        for (i in grids.indices) {
+            val grid = grids[i]
+            val gridState = states[i]
+            val markingStatus = markNumber(grid, gridState, num)
+            if (markingStatus != NotFullyMarked) {
+                return Result(num, grid, gridState, markingStatus)
+            }
+        }
+    }
+    throw IllegalStateException("no winner detected")
+}
+
+fun markNumber(grid: List<List<Int>>, gridState: List<MutableList<Boolean>>, number: Int): MarkingStatus {
+    var marked = false
+    for (i in grid.indices) {
+        val row = grid[i]
+        for (j in row.indices) {
+            if (row[j] == number) {
+                gridState[i][j] = true
+                marked = true
+            }
+        }
+    }
+    if (marked) {
+        return findMarkingStatus(gridState)
+    }
+    return NotFullyMarked
+}
+
+fun findMarkingStatus(gridState: List<MutableList<Boolean>>): MarkingStatus {
+    // check rows
+    for (i in gridState.indices) {
+        val row = gridState[i]
+        if (row.all { it }) {
+            return RowMarked(i)
+        }
+    }
+    // check columns
+    for (i in gridState[0].indices) { // doesnt matter though as they are 5x5
+        var allTrue = true
+        for (j in gridState.indices) {
+            if (!gridState[j][i]) {
+                allTrue = false
+                continue
+            }
+        }
+        if (allTrue) {
+            return ColumnMarked(i)
+        }
+    }
+    return NotFullyMarked
+}
+
+private fun calculateUnmarkedSum(grid: List<List<Int>>, gridState: List<MutableList<Boolean>>): Int {
+    var sum = 0
+    for(i in grid.indices) {
+        val row = grid[i]
+        for(j in row.indices) {
+            if (!gridState[i][j]) {
+                sum += grid[i][j]
+            }
+        }
+    }
+    return sum
+}
+
+fun main() {
+    val input = loadFromResources("day4.txt").readLines()
+    val split = input.splitWhen { it.trim().isEmpty() }
+    val numbers = split[0][0].split(",").map(String::toInt)
+    val grids = split.slice(1 until split.size)
+        .map { it.map { str -> str.split("\\s+".toRegex()).filter(String::isNotEmpty).map(String::toInt) } }
+    val res = findWinningGrid(numbers, grids)
+    val unmarkedSum = calculateUnmarkedSum(res.grid, res.gridState)
+    println(res.number * unmarkedSum)
+
+}
+
+
+
--- src/main/kotlin/com/sad/aoc2021/util.kt
+++ src/main/kotlin/com/sad/aoc2021/util.kt
@@ -7,3 +7,25 @@ fun loadFromResources(fileName: String): File {
 }
 
 fun File.readFirstLine(): String = this.readLines()[0]
+
+fun <T> List<T>.splitWhen(predicate: (T) -> Boolean): List<List<T>> {
+    val res = mutableListOf<List<T>>()
+    var currentList: MutableList<T>? = null
+    for (el in this) {
+        if (predicate(el)) {
+            if (currentList != null) {
+                res.add(currentList)
+            }
+            currentList = null
+        } else {
+            if (currentList == null) {
+                currentList = mutableListOf()
+            }
+            currentList.add(el)
+        }
+    }
+    if (currentList != null) {
+        res.add(currentList)
+    }
+    return res
+}

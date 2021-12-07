47baf29228ab2c8ecb952f30f48401669375e5e2
Sergey Dubakov <serge.dubakov@gmail.com>
2021-12-07 20:01:24 +0000

    day 7 part 2

--- src/main/kotlin/com/sad/aoc2021/day7/main.kt
+++ src/main/kotlin/com/sad/aoc2021/day7/main.kt
@@ -5,12 +5,12 @@ import com.sad.aoc2021.readFirstLine
 
 private fun alignmentCost(input: List<Int>, pos: Int) = input.map { Math.abs(it-pos) }.sum()
 
-private fun findBestAlignment(input: List<Int>): Pair<Int, Int> {
+private fun findBestAlignment(input: List<Int>, costFn: (List<Int>, Int) -> Int): Pair<Int, Int> {
     val maxPos = input.maxOrNull()!!
     var bestCost: Int? = null
     var bestPos: Int? = null
     for (i in 0..maxPos) {
-        val cost = alignmentCost(input, i)
+        val cost = costFn(input, i)
         if (bestCost == null || cost < bestCost) {
             bestCost = cost
             bestPos = i
@@ -19,9 +19,18 @@ private fun findBestAlignment(input: List<Int>): Pair<Int, Int> {
     return Pair(bestPos!!, bestCost!!)
 }
 
+private fun crabMovementCost(distance: Int) = (1..distance).sum()
+
+private fun alignmentCost2(input: List<Int>, pos: Int) = input.map { crabMovementCost(Math.abs(it-pos)) }.sum()
+
 fun main() {
     val input = loadFromResources("day7.txt").readFirstLine().split(",").map(String::toInt)
-    println(findBestAlignment(input))
+
+    // part 1
+    println(findBestAlignment(input, ::alignmentCost))
+
+    // part 2
+    println(findBestAlignment(input, ::alignmentCost2))
 }
 
 

1fc3dcdfa4cba429c0c5dd6f6d15c3c0f9eebcab
Sergey Dubakov <serge.dubakov@gmail.com>
2021-12-10 19:06:01 +0000

    day 10 part 2

--- src/main/kotlin/com/sad/aoc2021/day10/main.kt
+++ src/main/kotlin/com/sad/aoc2021/day10/main.kt
@@ -4,8 +4,9 @@ import com.sad.aoc2021.loadFromResources
 
 val closeToOpenMapping = mapOf(')' to '(', ']' to '[', '}' to '{', '>' to '<')
 val points = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
+val completionPoints = mapOf('(' to 1, '[' to 2, '{' to 3, '<' to 4)
 
-private fun firstIllegalChar(s: String): Char? {
+private fun firstIllegalCharAndIncompletePart(s: String): Pair<Char?, List<Char>> {
     val stack = mutableListOf<Char>()
     s.forEach { ch ->
         when (ch) {
@@ -14,15 +15,30 @@ private fun firstIllegalChar(s: String): Char? {
                 val shouldOpenWith = closeToOpenMapping[ch]
                 val lastOpen = stack.removeAt(stack.size - 1)
                 if (shouldOpenWith != lastOpen) {
-                    return ch
+                    return (ch to stack)
                 }
             }
         }
     }
-    return null
+    return (null to stack)
+}
+
+private fun completionScore(l: List<Char>): Long {
+    var res = 0L
+    for (i in l.size - 1 downTo 0) {
+        res = 5 * res + completionPoints[l[i]]!!
+    }
+    return res
 }
 
 fun main() {
     val input = loadFromResources("day10.txt").readLines()
-    println(input.map(::firstIllegalChar).map { points[it] ?: 0 }.sum())
+
+    // part 1
+    println(input.map { firstIllegalCharAndIncompletePart(it).first }.map { points[it] ?: 0 }.sum())
+
+    // part 2
+    val incomplete = input.map(::firstIllegalCharAndIncompletePart).filter { it.first == null }.map { it.second }
+    val completionScores = incomplete.map(::completionScore).sorted()
+    println(completionScores[completionScores.size / 2])
 }

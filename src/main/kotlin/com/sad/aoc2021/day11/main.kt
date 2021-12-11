9eeef4feb04e9dd885cefeba7a428c722d5c7196
Sergey Dubakov <serge.dubakov@gmail.com>
2021-12-11 15:39:33 +0000

    day 11 part 1

---
+++ src/main/kotlin/com/sad/aoc2021/day11/main.kt
@@ -0,0 +1,72 @@
+package com.sad.aoc2021.day11
+
+import com.sad.aoc2021.loadFromResources
+
+fun findAdjacent(i: Int, j: Int, input: List<List<Int>>): List<Pair<Int, Int>> {
+    return arrayOf(
+        Pair(i - 1, j), // up
+        Pair(i + 1, j), // down
+        Pair(i, j - 1), // left
+        Pair(i, j + 1), // right
+        Pair(i + 1, j + 1),
+        Pair(i + 1, j - 1),
+        Pair(i - 1, j + 1),
+        Pair(i - 1, j - 1)
+    ).filter { (row, col) ->
+        row >= 0 && row < input.size && col >= 0 && col < input[0].size
+    }
+}
+
+fun light(input: MutableList<MutableList<Int>>, i: Int, j: Int) {
+    input[i][j] = 0
+    val adjacent = findAdjacent(i, j, input)
+    for ((adjr, adjc) in adjacent) {
+        if (input[adjr][adjc] != 0) {
+            if (input[adjr][adjc] != 10) {
+                input[adjr][adjc] = input[adjr][adjc] + 1
+            }
+            if (input[adjr][adjc] == 10) {
+                light(input, adjr, adjc)
+            }
+        }
+    }
+}
+
+fun simulateStep(input: List<List<Int>>): List<List<Int>> {
+    // increase by 1
+    var mutableMap = input.map {
+        it.map {
+            it + 1
+        }.toMutableList()
+    }.toMutableList()
+    for (i in mutableMap.indices) {
+        val row = mutableMap[i]
+        for (j in row.indices) {
+            if (mutableMap[i][j] == 10) {
+                light(mutableMap, i, j)
+            }
+        }
+    }
+    return mutableMap
+}
+
+fun simulateSteps(input: List<List<Int>>, steps: Int): Int {
+    var map = input
+    var lights = 0
+    for (i in 1..steps) {
+        map = simulateStep(map)
+        lights += map.map { it.map { v -> if (v == 0) 1 else 0 }.sum() }.sum()
+    }
+    return lights
+}
+
+fun main() {
+    val input = loadFromResources("day11.txt").readLines().map {
+        it.map { ch ->
+            ch.digitToInt()
+        }
+    }
+//    val after1step = simulateStep(simulateStep(input))
+//    println(after1step.map { it.joinToString(separator = "") }.joinToString(separator = "\n"))
+    println(simulateSteps(input, 100))
+}

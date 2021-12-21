20fd7edf5362f1a992943214b75c400ac02aaa62
Sergey Dubakov <serge.dubakov@gmail.com>
2021-12-21 21:31:56 +0000

    day 21 part 2

--- src/main/kotlin/com/sad/aoc2021/day21/main.kt
+++ src/main/kotlin/com/sad/aoc2021/day21/main.kt
@@ -13,20 +13,23 @@ data class GameState(
     fun isWon(): Boolean = p1score >= 1000 || p2score >= 1000
 }
 
+data class GameSummary(val p1wins: Long, val p2wins: Long) {
+    operator fun plus(that: GameSummary) = GameSummary(p1wins + that.p1wins, p2wins + that.p2wins)
+}
+
+fun roll(prevRoll: Int): Int {
+    var roll = prevRoll + 1
+    if (roll == 101) {
+        roll = 1
+    }
+    return roll
+}
+
+fun updatePos(pos: Int, roll: Int): Int {
+    return ((pos - 1 + roll) % 10) + 1
+}
+
 fun play(state: GameState, player: Int): GameState {
-    fun roll(prevRoll: Int): Int {
-        var roll = prevRoll + 1
-        if (roll == 101) {
-            roll = 1
-        }
-        println("rolled ${roll}")
-        return roll
-    }
-
-    fun updatePos(pos: Int, roll: Int): Int {
-        return ((pos-1 + roll) % 10) + 1
-    }
-
     val roll1 = roll(state.lastRoll)
     val roll2 = roll(roll1)
     val roll3 = roll(roll2)
@@ -44,9 +47,77 @@ fun play(state: GameState, player: Int): GameState {
     }
 }
 
+data class DiracGameState(
+    val p1pos: Int,
+    val p2pos: Int,
+    val p1score: Int,
+    val p2score: Int
+) {
+    fun isWon(): Boolean = p1score >= 21 || p2score >= 21
+}
+
+val diracRolls = generateDiracRolls()
+
+fun generateDiracRolls(): List<Int> {
+    val rolls = mutableListOf<Int>()
+    for (i in 1..3) {
+        for (j in 1..3) {
+            for (k in 1..3) {
+                rolls.add(i + j + k)
+            }
+        }
+    }
+    return rolls
+}
+
+val cache = mutableMapOf<Pair<DiracGameState, Int>, GameSummary>()
+
+fun playDiracCached(state: DiracGameState, player: Int): GameSummary {
+    val key = Pair(state, player)
+    var gs = cache[key]
+    if (gs == null) {
+        gs = playDirac(state, player)
+        cache.put(key, gs)
+    }
+    return gs
+}
+
+fun playDirac(state: DiracGameState, player: Int): GameSummary {
+    fun playPlayer1(roll: Int): GameSummary {
+        val pos = updatePos(state.p1pos, roll)
+        val newState = DiracGameState(pos, state.p2pos, state.p1score + pos, state.p2score)
+        val summary: GameSummary
+        if (newState.isWon()) {
+            summary = GameSummary(1, 0)
+        } else {
+            summary = playDiracCached(newState, 2)
+        }
+        return summary
+    }
+
+    fun playPlayer2(roll: Int): GameSummary {
+        val pos = updatePos(state.p2pos, roll)
+        val newState = DiracGameState(state.p1pos, pos, state.p1score, state.p2score + pos)
+        val summary: GameSummary
+        if (newState.isWon()) {
+            summary = GameSummary(0, 1)
+        } else {
+            summary = playDiracCached(newState, 1)
+        }
+        return summary
+    }
+
+    if (player == 1) {
+        return diracRolls.map(::playPlayer1).reduce { gs1, gs2 -> gs1 + gs2 }
+    } else {
+        return diracRolls.map(::playPlayer2).reduce { gs1, gs2 -> gs1 + gs2 }
+    }
+}
+
 fun main() {
     val input = loadFromResources("day21.txt").readLines().map { it.split(": ")[1].toInt() }
-    println(input)
+
+    // part 1
     var state = GameState(input[0], input[1], 0, 0, 0, 0)
     while (true) {
         state = play(state, 1)
@@ -60,4 +131,9 @@ fun main() {
             break
         }
     }
+
+    // part 2
+    val diracState = DiracGameState(input[0], input[1], 0, 0)
+    println(playDirac(diracState, 1))
+
 }
